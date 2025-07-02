09105149626f60eb4a3a5a32e189c858c5529e7b
fymotools <fymotools@gmail.com>
2025-07-02 04:36:56 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['via.placeholder.com'], // Add any other external image domains you use
+  },
+};
+
+module.exports = nextConfig;
